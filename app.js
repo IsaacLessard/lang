4d14ac80f02fcabe23df546da527ae25270df8e4
Isaac Lessard <IsaacL@Isaacs-MacBook-Air-2.local>
2015-11-09 14:10:17 -0700

    initial files

---
+++ app.js
@@ -0,0 +1,11 @@
+function getData(){
+
+
+}
+
+
+
+getData('GET',
+
+urlApp = "https://leonardoscorza-translate-text-v1.p.mashape.com/translate.json?q="+
+      query+"&api-key=HOlIxpOFqKmshb0W1BUWzk8e83uBp1LsMpRjsnWuKJfY4TFBtl";')
